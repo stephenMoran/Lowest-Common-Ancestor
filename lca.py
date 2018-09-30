33e27cdec469d61781aa368f4919fd8a38fe79a5
stephenMoran <32431967+stephenMoran@users.noreply.github.com>
2018-09-30 15:45:24 +0100

    Implementation of findLCA function

--- lca.py
+++ lca.py
@@ -1,5 +1,5 @@
 # Python Program to find the lowest common ancestor (LCA) of a Binary Tree
-
+#O(n) runtime
 #Binary tree node
 class Node:
     #constructor
@@ -29,3 +29,25 @@ def findPath( root, path, keyToFind):
     #if path cannot be found empty the array and exit
     path.pop()
     return False
+
+
+#Fucntion that determiens the lowest common ancestor
+def findLCA(root, n1, n2):
+
+    # To store paths to n1 and n2 fromthe root
+    path1 = []
+    path2 = []
+
+    #If paths cannot be found exit
+    if (not findPath(root, path1, n1) or not findPath(root, path2, n2)):
+        return -1
+
+    # Compare the paths to get the first different value
+    i = 0
+    while(i < len(path1) and i < len(path2)):
+        #when paths diverge we know that the LCA has been passed
+        if path1[i] != path2[i]:
+            break
+        i += 1
+    #backstep to find LCA va;ue and return
+    return path1[i-1]
