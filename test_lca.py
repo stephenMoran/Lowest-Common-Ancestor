24d437508b4673b9e872ae28f2f7c66ee885b74b
stephenMoran <32431967+stephenMoran@users.noreply.github.com>
2018-09-30 16:01:52 +0100

    Test 1 of findLCA success

--- test_lca.py
+++ test_lca.py
@@ -1,7 +1,7 @@
 import pytest
 import lca
 
-#simple binary tree test
+#simple binary tree test. Testing right subtree
 def test_pathFinder1():
     path = []
     root = lca.Node(1)
@@ -9,8 +9,35 @@ def test_pathFinder1():
     root.right = lca.Node(3)
     assert lca.findPath(root, path, 3) == True
 
-#Testing with null root
+#simple binary tree test. Testing left subtree
 def test_pathFinder2():
+    path = []
+    root = lca.Node(1)
+    root.left = lca.Node(2)
+    root.right = lca.Node(3)
+    assert lca.findPath(root, path, 2) == True
+
+#Testing with null root
+def test_pathFinder3():
     path = []
     root = None
     assert lca.findPath(root, path, 3) == False
+
+#Testing with larger tree
+def test_pathFinder4():
+    path = []
+    root = lca.Node(1)
+    root.left = lca.Node(2)
+    root.right = lca.Node(3)
+    root.left.left = lca.Node(4)
+    root.left.right = lca.Node(5)
+    root.right.right = lca.Node(6)
+    root.right.left = lca.Node(7)
+    assert lca.findPath(root, path , 7) == True
+
+#Testing with simple bianry tree
+def test_lcaFind():
+    root = lca.Node(1)
+    root.left = lca.Node(2)
+    root.right = lca.Node(3)
+    assert lca.findLCA(root, 2, 3) == 1
